39c82c82f6dee0a5d0cac725f931cb43cb066c9a
sonms <smsms5676@naver.com>
2026-04-24 16:16:47 +0900

    [setting#1] core:localstorage 모듈 추가 및 core:common 설정 변경
    - `domain` 모듈의 기존 소스 코드 및 빌드 설정을 삭제하고 디렉토리 유지를 위해 `.gitkeep`을 추가하였습니다.
    - `core:localstorage` 모듈을 신규 생성하고 관련 Gradle 및 Manifest 설정을 추가하였습니다.
    - `core:common` 모듈에 `focpet.android.compose` 컨벤션 플러그인을 추가하였습니다.

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -2,6 +2,7 @@ import com.focpet.buildlogic.setNamespace
 
 plugins {
     alias(libs.plugins.focpet.android.library)
+    alias(libs.plugins.focpet.android.compose)
 }
 
 android {
--- domain/src/main/java/com/focpet/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.focpet.domain
-
-class MyClass {
-}
